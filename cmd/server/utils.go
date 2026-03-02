e1fc750657bd161b896a7f49598e1463f82379b2
mahmut <AymenJacquet@outlook.com>
2026-03-02 06:45:39 +0000

    feat(build): print startup version metadata and use injected commit

--- cmd/server/utils.go
+++ cmd/server/utils.go
@@ -1,22 +1,10 @@
 package main
 
 import (
-	"os/exec"
 	"runtime"
-	"strings"
 
 	"github.com/sirupsen/logrus"
 )
-
-// runGitCommand runs a git command and returns the output
-func runGitCommand(args ...string) (string, error) {
-	cmd := exec.Command("git", args...)
-	output, err := cmd.Output()
-	if err != nil {
-		return "", err
-	}
-	return strings.TrimSpace(string(output)), nil
-}
 
 // setupGOMAXPROCS sets reasonable defaults for GOMAXPROCS
 func setupGOMAXPROCS() {
